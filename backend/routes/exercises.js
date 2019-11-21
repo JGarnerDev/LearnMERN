ab9d6f2067191b63168c549fc4b6306500f5ac6f
Jeffe <j.garner200@gmail.com>
2019-11-21 18:00:56 -0500

    -

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -33,7 +33,7 @@ router.route('/:id').get((req, res) => {
 
 router.route('/:id').delete((req, res) => {
     Exercise.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Exercise deleted: '))
+    .then(() => res.json('Exercise deleted'))
     .catch(err => res.status(500).json('Error: ' + err))
 })
 
